4f369cfe769b9d0c6259e0cd285884d35d5356a7
Cobby <39762158+Cobbi@users.noreply.github.com>
2023-04-10 05:10:53 +0000

    Update sum_even_numbers.c

--- recursion-in_c/sum_even_numbers.c
+++ recursion-in_c/sum_even_numbers.c
@@ -1,7 +1,7 @@
 // Ask the user to enter a positive number of an array, then we sum the even numbers in the array.
 #include <stdio.h>
 
-void SumOfEven(int a[], int num, int sum);
+void sumOfEven(int a[], int num, int sum);
 
 int main(int argc, char const *argv[])
 {
@@ -14,16 +14,16 @@ int main(int argc, char const *argv[])
 	for(i=0; i<num; i++){
 		scanf("%d", &a[i]);
 	}
-	SumOfEven(a, num-1, sum);
+	sumOfEven(a, num-1, sum);
 	return 0;
 }
 
-void SumOfEven(int a[], int num, int sum) {
+void sumOfEven(int a[], int num, int sum) {
 	if (num >= 0) {
 		if((a[num])%2 == 0) {
 			sum+=(a[num]);
 		}
-		SumOfEven(a, num-1, sum);
+		sumOfEven(a, num-1, sum);
 	}
 	else {
 		printf("Sum=%d\n", sum);
